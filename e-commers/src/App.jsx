c63c895464f8ed5d306578948ca88c16bb3cb34f
fedeacu <fede-acu-1994@hotmail.com>
2024-05-22 21:01:43 -0300

    correccion

--- e-commers/src/App.jsx
+++ e-commers/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ProductPage from './pages/ProductPage';
@@ -17,7 +17,7 @@ function App() {
           <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Router>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
